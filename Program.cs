80baceb435a188de69b9c4f967c616b9caa7ee65
Dmytro <dimonbog2004@gmail.com>
2023-12-11 09:39:59 +0200

    Query2 done

--- Program.cs
+++ Program.cs
@@ -83,13 +83,10 @@ namespace Practice_Linq
         {
             //Query 3: Вивести всі домашні матчі збірної Франції за 2021 рік, де вона зіграла у нічию.
 
-            var selectedGames = games
-                .Where(game => game.Country == "France" && game.Home_team == "France" && game.Date.Year == 2021 && game.Home_score == game.Away_score);   // Корегуємо запит !!!
+            var selectedGames = games;   // Корегуємо запит !!!
 
             // Перевірка
             Console.WriteLine("\n======================== QUERY 3 ========================");
-
-            foreach (FootballGame game in selectedGames) { PrintMatch(game); }
         }
 
         // Запит 4
@@ -97,14 +94,11 @@ namespace Practice_Linq
         {
             //Query 4: Вивести всі матчі збірної Германії з 2018 року по 2020 рік (включно), в яких вона на виїзді програла.
 
-            var selectedGames = games
-                .Where(game => game.Away_team == "Germany" && game.Date.Year >= 2018 && game.Date.Year <= 2020 && game.Home_score > game.Away_score);   // Корегуємо запит !!!
+            var selectedGames = games;   // Корегуємо запит !!!
 
 
             // Перевірка
             Console.WriteLine("\n======================== QUERY 4 ========================");
-
-            foreach (FootballGame game in selectedGames) { PrintMatch(game); }
         }
 
         // Запит 5
@@ -113,15 +107,11 @@ namespace Practice_Linq
             //Query 5: Вивести всі кваліфікаційні матчі (UEFA Euro qualification), які відбулися у Києві чи у Харкові, а також за умови перемоги української збірної.
 
 
-            var selectedGames = games.Where(game => game.Tournament == "UEFA Euro qualification" &&
-                               (game.City == "Kyiv" || game.City == "Kharkiv") &&
-                               (game.Home_team == "Ukraine" && game.Home_score > game.Away_score || game.Away_team == "Ukraine" && game.Away_score > game.Home_score));  // Корегуємо запит !!!
+            var selectedGames = games;  // Корегуємо запит !!!
 
 
             // Перевірка
             Console.WriteLine("\n======================== QUERY 5 ========================");
-
-            foreach (FootballGame game in selectedGames) { PrintMatch(game); }
         }
 
         // Запит 6
@@ -130,15 +120,11 @@ namespace Practice_Linq
             //Query 6: Вивести всі матчі останнього чемпіоната світу з футболу (FIFA World Cup), починаючи з чвертьфіналів (тобто останні 8 матчів).
             //Матчі мають відображатися від фіналу до чвертьфіналів (тобто у зворотній послідовності).
 
-            var selectedGames = games.Where(game => game.Tournament == "FIFA World Cup")
-                .OrderByDescending(game => game.Date)
-                .Take(8);   // Корегуємо запит !!!
+            var selectedGames = games;   // Корегуємо запит !!!
 
 
             // Перевірка
             Console.WriteLine("\n======================== QUERY 6 ========================");
-
-            foreach (FootballGame game in selectedGames) { PrintMatch(game); }
         }
 
         // Запит 7
@@ -146,14 +132,9 @@ namespace Practice_Linq
         {
             //Query 7: Вивести перший матч у 2023 році, в якому збірна України виграла.
 
-            FootballGame game = games.Where(game => game.Date.Year == 2023 &&
-                               ((game.Home_team == "Ukraine" && game.Home_score > game.Away_score) || 
-                               (game.Away_team == "Ukraine" && game.Away_score > game.Home_score)))
-                .OrderBy(game => game.Date).First();   // Корегуємо запит !!!
+            FootballGame game = null;   // Корегуємо запит !!!
             // Перевірка
             Console.WriteLine("\n======================== QUERY 7 ========================");
-
-            PrintMatch(game);
         }
 
         // Запит 8
@@ -162,22 +143,10 @@ namespace Practice_Linq
             //Query 8: Перетворити всі матчі Євро-2012 (UEFA Euro), які відбулися в Україні, на матчі з наступними властивостями:
             // MatchYear - рік матчу, Team1 - назва приймаючої команди, Team2 - назва гостьової команди, Goals - сума всіх голів за матч
 
-            var selectedGames = games.Where(game => game.Tournament == "UEFA Euro" && game.Country == "Ukraine")
-                .Select(game => new
-                {
-                    MatchYear = game.Date.Year,
-                    Team1 = game.Home_team,
-                    Team2 = game.Away_team,
-                    Goals = game.Home_score + game.Away_score
-                });   // Корегуємо запит !!!
+            var selectedGames = games;   // Корегуємо запит !!!
 
             // Перевірка
             Console.WriteLine("\n======================== QUERY 8 ========================");
-
-            foreach (var game in selectedGames)
-            {
-                Console.WriteLine($"{game.MatchYear} {game.Team1} - {game.Team2}, Goals: {game.Goals}");
-            }
         }
 
 
@@ -187,32 +156,10 @@ namespace Practice_Linq
             //Query 9: Перетворити всі матчі UEFA Nations League у 2023 році на матчі з наступними властивостями:
             // MatchYear - рік матчу, Game - назви обох команд через дефіс (першою - Home_team), Result - результат для першої команди (Win, Loss, Draw)
 
-            var selectedGames = games.Where(game => game.Tournament == "UEFA Nations League" && game.Date.Year == 2023)
-                .Select(game => new
-                {
-                    MatchYear = game.Date.Year,
-                    Game = $"{game.Home_team}-{game.Away_team}",
-                    Result = GetResultForTeam1(game)
-                });   // Корегуємо запит !!!
+            var selectedGames = games;   // Корегуємо запит !!!
 
             // Перевірка
             Console.WriteLine("\n======================== QUERY 9 ========================");
-
-            foreach(var game in selectedGames)
-            {
-                Console.WriteLine($"{game.MatchYear} {game.Game}, Result for team1: {game.Result}");
-            }
-        }
-
-        static string GetResultForTeam1(FootballGame game) //Так простіше, отже я додав функцію.
-                                                           //До речі, у Kotlin можна було б напряму вставити if без використання функції
-        {
-            if (game.Home_score > game.Away_score)
-                return "Win";
-            else if (game.Home_score < game.Away_score)
-                return "Loss";
-            else
-                return "Draw";
         }
 
         // Запит 10
@@ -220,15 +167,10 @@ namespace Practice_Linq
         {
             //Query 10: Вивести з 5-го по 10-тий (включно) матчі Gold Cup, які відбулися у липні 2023 р.
 
-            var selectedGames = games
-                .Where(game => game.Tournament == "Gold Cup" && game.Date.Year == 2023 && game.Date.Month == 7)
-                .Skip(4)
-                .Take(6); // Корегуємо запит !!!
+            var selectedGames = games; // Корегуємо запит !!!
 
             // Перевірка
             Console.WriteLine("\n======================== QUERY 10 ========================");
-
-            foreach (FootballGame game in selectedGames) { PrintMatch(game); }
         }
 
     }
